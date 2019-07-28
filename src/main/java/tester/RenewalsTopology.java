0178562ad2b51e4e7f65927a9416f8cd79adb033
pete knight <pete@knightech.net>
2019-07-28 21:33:33 +0100

    variation

--- src/main/java/tester/RenewalsTopology.java
+++ src/main/java/tester/RenewalsTopology.java
@@ -15,9 +15,11 @@
  */
 package tester;
 
+import jdk.internal.joptsimple.util.KeyValuePair;
 import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.kstream.Consumed;
